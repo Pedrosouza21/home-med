a9a8448ba7d0f40d010c2b9da96993617a7428e3
Pedro Souza <pedrohenriqueoliveiradesouza9@gmail.com>
2026-07-21 21:30:51 -0300

    feat: configura servidor Express e conexão com PostgreSQL

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,36 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+
+const pool = require("./database/connection");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.listen(3001, () => {
-    console.log("Servidor rodando");
+app.get("/", (req, res) => {
+    res.send("API HomeMed funcionando!");
 });
+
+const iniciarServidor = async () => {
+    try {
+
+        await pool.query("SELECT NOW()");
+
+        console.log("✅ Banco conectado!");
+
+        app.listen(process.env.PORT, () => {
+            console.log(`🚀 Servidor rodando na porta ${process.env.PORT}`);
+        });
+
+    } catch (erro) {
+
+        console.error("❌ Erro ao conectar no banco");
+
+        console.error(erro);
+
+    }
+}
+
+iniciarServidor();
